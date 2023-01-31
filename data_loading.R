27abfcc174e2ba95c033dea0ff607dd485e56759
chuembucket <cchuemmler@gmail.com>
2023-01-31 14:20:17 -0500

    data loading file additions

--- data_loading.R
+++ data_loading.R
@@ -3,11 +3,20 @@
 library(here)
 library(tidyverse)
 library(sf)
+library(readxl)
 
 data_folder <- file.path(here() %>% dirname(), 'data')
 
 
 #stops
 stops <- read.csv(paste(data_folder, "/stops.csv", sep = ''))
+
+#totalboardings2021
+total_boardings_jan <- read_excel(paste(data_folder, "/totalboardings2021.xlsx", sep = ""),
+                                  sheet = "Jan-Aug")
+
+
+total_boardings_sep <- read_excel(paste(data_folder, "/totalboardings2021.xlsx", sep = ""),
+                                  sheet = "Sept - Dec")
 
 
