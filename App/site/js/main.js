3c1b5312d0fb261b99c135d33639bfd2eb6306c3
YingxueOu(Sprite) <oyx@upenn.edu>
2023-04-27 14:09:56 -0400

    App updated

--- App/site/js/main.js
+++ App/site/js/main.js
@@ -1,34 +1,91 @@
 
-import { initMap, addStopsLayer} from './map.js';
+import { initMap} from './map.js';
+import {addStopsAndRoutesLayer } from './current_data.js';
 import { addHexLayer } from './model.js';
 
 
 const epMap = initMap();
 
-// Get the routes checkbox element
-const checkbox_Routes = document.getElementById("myCheck_Routes");
+// create a button to show the ridership data
+const ridershipButton = document.getElementById("ridership-button");
+const routesSelect = document.getElementById('routes-select');
 
-// Add a click event listener to the checkbox
-checkbox_Routes.addEventListener("click", function() {
-  // Check if the checkbox is checked
-  if (checkbox_Routes.checked) {
-    // Call the addStopsLayer function with the specified arguments
-    addStopsLayer('./data/ridership.geojson', epMap);
-  } 
+ridershipButton.addEventListener("click", function() {
+    routesSelect.style.display = 'block';
+    addStopsAndRoutesLayer('./data/ridership.geojson', './data/transit_lines.geojson', epMap)
+
+    var tabContainer = document.querySelector(".tab-container");
+    if (tabContainer.style.display === "none") {
+      tabContainer.style.display = "block";
+    }
 });
 
 
-// Get the hex checkbox element
-const checkbox_Hex = document.getElementById("myCheck_Hex");
+// Define a variable to keep track of the hex layer
+let hexLayer = null;
 
-// Add a click event listener to the checkbox
-checkbox_Hex.addEventListener("click", function() {
-  // Check if the checkbox is checked
-  if (checkbox_Hex.checked) {
-    // Call the addStopsLayer function with the specified arguments
-    addHexLayer('./data/final_hex.geojson', epMap);
-  }
+// Get the hex button element
+const hexButton = document.getElementById("hex-button");
+
+// Add a click event listener to the hex button
+hexButton.addEventListener("click", function() {
+    if (hexLayer) {
+        // If the hex layer is already added, remove it and set the hexLayer variable to null
+        epMap.removeLayer(hexLayer);
+        hexLayer = null;
+    } else {
+        // If the hex layer is not added, call the addHexLayer function and set the hexLayer variable to the returned layer
+        hexLayer = addHexLayer('./data/final_hex.geojson', epMap);
+    }
 });
+
+
+
+// visit our GitHub webpage
+const githubButton = document.getElementById('github-button');
+  githubButton.addEventListener('click', () => {
+    const confirmation = confirm('You are about to visit our GitHub page. Are you sure you want to continue?');
+    if (confirmation) {
+      window.open('https://github.com/jtrummler/ElPaso-Bus-Network', '_blank');
+    }
+  });
+
+
+
+// import new routes
+const importButton = document.getElementById('import-button');
+  const fileInput = document.getElementById('file-input');
+
+  importButton.addEventListener('click', () => {
+    fileInput.click();
+  });
+
+  fileInput.addEventListener('change', () => {
+    const file = fileInput.files[0];
+    const accessToken = 'YOUR_DROPBOX_ACCESS_TOKEN';
+    const url = 'https://content.dropboxapi.com/2/files/upload';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Authorization', `Bearer ${accessToken}`);
+    xhr.setRequestHeader('Content-Type', 'application/octet-stream');
+    xhr.setRequestHeader('Dropbox-API-Arg', JSON.stringify({
+      path: `/${file.name}`,
+      mode: 'add',
+      autorename: true,
+      mute: false
+    }));
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        alert('File uploaded successfully!');
+      } else {
+        alert('An error occurred while uploading the file.');
+      }
+    };
+
+    xhr.send(file);
+  });
 
 
 
--- App/site/js/model.js
+++ App/site/js/model.js
@@ -2,16 +2,25 @@ function addHexLayer(filePath, map) {
     fetch(filePath)
     .then(response => response.json())
     .then(data => {
-        logGeoJSONData(data);
+        console.log(data);
 
         // Find the maximum and minimum ridership values in the dataset
-        const ridershipValues = data.features.map(feature => feature.properties.ridership);
-        const maxRidership = Math.max(...ridershipValues);
-        const minRidership = Math.min(...ridershipValues);
+        const predictedRider = data.features.map(feature => feature.properties.prediction
+          );
+        console.log(predictedRider);
+        const currentRider = data.features.map(feature => feature.properties.ridership);
+        console.log(currentRider);
+        const maxRidership = Math.max(...predictedRider);
+        const minRidership = Math.min(...predictedRider);
 
+        // Define a variable to hold the reference to the BI element in the sidebar
+        const bi = document.getElementById('BI');  
+        const actualChart = document.getElementById('actualChart');
+
+        // Define the hex color based on the predicted ridership
         const geojsonLayer = L.geoJSON(data, {
             style: function(feature) {
-                const ridership = feature.properties.ridership;
+                const ridership = feature.properties.prediction;
                 // Convert the ridership value to a percentage of the range between min and max
                 const percent = (ridership - minRidership) / (maxRidership - minRidership);
                 // Calculate the red, green, and blue components of the fill color
@@ -27,8 +36,28 @@ function addHexLayer(filePath, map) {
             },
             onEachFeature: function(feature, layer) {
                 // Bind a popup to each hexagon displaying the name of the GeoJSON feature
-                layer.bindPopup(feature.properties.name);
-            }
+                layer.bindPopup("<br>Current ridership: "+Math.round(feature.properties.ridership));
+                layer.bindPopup("<br>Predicted ridership: "+Math.round(feature.properties.prediction));
+      
+                // Bind a click event handler to each hexagon to show the age distribution pie chart
+                layer.on('click', function() {
+                    racialChart(feature);
+                    pctCarChart(feature);
+                    amenitiesChart(feature);
+
+                    // Update the content of the BI element with the property values
+                    bi.innerHTML = '<h2>Selected Hexagon</h2>' +
+                    '<p>Total Housing Units: ' + Math.round(feature.properties.totalHU) + '</p>' +
+                    '<p>Total Jobs: ' + Math.round(feature.properties.total_jobs) + '</p>' +
+                    '<p>Median Houshold Income: ' + Math.round(feature.properties.medHHInc) + '</p>' +
+                    '<p>Disability: ' + Math.round(feature.properties.disability) + '</p>' ;
+
+                    // plot the jitter
+                    jitterPlot(currentRider, feature.properties.ridership, 'currentChart', 'Current Ridership');
+                    jitterPlot(predictedRider, feature.properties.prediction, 'predictedChart', 'Predict Ridership');
+
+                });
+              }
         });
         geojsonLayer.addTo(map);
     })
@@ -36,6 +65,381 @@ function addHexLayer(filePath, map) {
 
     return map;
 }
+
+
+function jitterPlot(ridership, select, chartId, title) {
+    const canvas = document.getElementById(chartId);
+  
+    const data = ridership.map((rider) => {
+      return {
+        x: Math.random() + 0.5,
+        y: rider,
+        color: rider === select ? 'orange' : 'rgba(255, 255, 255, 0.5)'
+      }
+    })
+  
+    const filteredData = data.filter((d) => d.y !== null);
+    canvas.width = canvas.width * 0.25;
+    canvas.height = canvas.height * 0.5;
+    
+    new Chart(canvas, {
+      type: 'scatter',
+      data: {
+        datasets: [
+          {
+            data: filteredData,
+            borderColor: 'transparent',
+            backgroundColor: filteredData.map(d => d.color),
+            pointRadius: 2,
+            pointHoverRadius: 5,
+            pointHitRadius: 20,
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        legend: {
+          display: false
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                  display: false,
+                  beginAtZero: true
+                },
+                gridLines: {
+                  display: false
+                }
+              }],
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true,
+                  display: false,
+                  callback: function (value) {
+                    return value.toLocaleString();
+                  },
+                  fontColor: '#fff' // add this line to change the color of yAxes
+                },
+                gridLines: {
+                  display: true
+                }
+              }]
+        },
+        tooltips: {
+          callbacks: {
+            label: function (tooltipItem, data) {
+              return tooltipItem.yLabel.toLocaleString();
+            }
+          }
+        },
+        title: {
+          display: true,
+          text: title,
+          fontColor:'#fff'
+        }
+      }
+    });
+  }
+  
+
+function racialChart(feature){
+
+    const racialChartCanvas = document.getElementById('racialChartCanvas');
+
+    racialChartCanvas.style.display = 'flex';
+    racialChartCanvas.style.height = racialChartCanvas.style.height * 1.5;
+    racialChartCanvas.style.width = racialChartCanvas.style.width * 0.5;
+
+    // Remove the existing chart if there is one
+    if (window.racialChart) {
+        window.racialChart.destroy();
+    }
+
+    const raceData = [
+      feature.properties.whitePop,
+        feature.properties.blackPop,
+        feature.properties.hlPop
+      ];
+
+    const raceLabels = ['White', 'Black', 'Hispanic'];
+    
+    window.raceChart = new Chart(racialChartCanvas, {
+    type: 'pie',
+    data: {
+        labels: raceLabels,
+        datasets: [{
+        data: raceData,
+        backgroundColor: ['#ffb545','#78938a', '#4A5899']
+        }]
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Races Distribution',
+            fontColor: '#fff'
+        },
+        legend: {
+            labels: {
+                fontColor: '#fff',
+                fontSize: 8,
+                boxWidth: 8,
+                boxHeight: 8
+            }
+        }
+    }
+    });
+
+}
+
+
+function pctCarChart(feature){
+    const carChartCanvas = document.getElementById('carChartCanvas');
+
+    carChartCanvas.style.display = 'flex';
+    carChartCanvas.style.height = carChartCanvas.style.height * 1.5;
+    carChartCanvas.style.width = carChartCanvas.style.width * 0.5;
+    // Check if the chart exists
+    if (window.carChart) {
+        // Destroy the chart if it exists
+        window.carChart.destroy();
+    }
+
+    const carData = [
+        feature.properties.pct0Car,
+        feature.properties.pct1Car,
+        feature.properties.pct2Car
+      ];
+
+    const carLabels = ['pct0Car', 'pct1Car', 'pct2Ca'];
+
+    window.carChart = new Chart(carChartCanvas, {
+        type: 'pie',
+        data: {
+          labels: carLabels,
+          datasets: [{
+            data: carData,
+            backgroundColor: ['#ffb545','#78938a', '#4A5899']
+          }]
+        },
+        options: {
+            responsive: true,
+            title: {
+                display: true,
+                text: 'Pct Car Distribution',
+                fontColor: '#fff'
+            },
+            legend: {
+                labels: {
+                    fontColor: '#fff',
+                    fontSize: 8,
+                    boxWidth: 8,
+                    boxHeight: 8
+                }
+            }
+        }
+    });
+}
+
+
+function occupationChart(feature){
+
+  const occupationData = [
+      feature.properties.NAICS11,
+      feature.properties.NAICS21,
+      feature.properties.NAICS22,
+      feature.properties.NAICS23,
+      feature.properties.NAICS31_33,
+      feature.properties.NAICS42,
+      feature.properties.NAICS44_46,
+      feature.properties.NAICS48_49,
+      feature.properties.NAICS51,
+      feature.properties.NAICS52,
+      feature.properties.NAICS53,
+      feature.properties.NAICS54,
+      feature.properties.NAICS55,
+      feature.properties.NAICS56,
+      feature.properties.NAICS61,
+      feature.properties.NAICS62,
+      feature.properties.NAICS71,
+      feature.properties.NAICS72,
+      feature.properties.NAICS81,
+      feature.properties.NAICS92
+  ]
+
+  const occupationLabels = [
+      '11', 
+      '21', 
+      '22',
+      '23', 
+      '31_33', 
+      '42',
+      '44_46',
+      '48_49', 
+      '51', 
+      '52',
+      '53', 
+      '54', 
+      '55',
+      '56', 
+      '61', 
+      '62',
+      '71',
+      '72', 
+      '81', 
+      '92',
+  ];
+
+
+  // Get the canvas element
+  const occChartCanvas = document.getElementById('occChartCanvas');
+
+  // Check if a chart already exists
+  if (window.occupationChart) {
+      window.occupationChart.destroy();
+  }
+
+  // Create the new chart
+  window.occupationChart = new Chart(occChartCanvas, {
+      type: 'bar',
+      data: {
+      labels: occupationLabels,
+      datasets: [{
+          data: occupationData,
+          backgroundColor: [
+              '#f7909f', '#e2647f', '#d1698c', '#e8555f', '#da332a', 
+              '#fe672a', '#ff7a34', '#ffb22c', '#83ba69', '#4ca64e',
+              '#2a897e', '#287f91', '#62b0dc', '#6492cb', '#4975c4', 
+              '#3b4f93', '#32436f', '#5f5395', '#6f4483', '#5a3340']
+      }]
+      },
+      options: {
+          responsive: true,
+          title: {
+              display: true,
+              text: 'NAICS Occupation distribution',
+              fontColor: '#fff'
+          },
+          scales: {
+              xAxes: [{
+                  ticks: {
+                      fontColor: '#fff'
+                  }
+              }],
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true,
+                      fontColor: '#fff'
+                  }
+              }]
+          },
+          legend: {
+              labels: {
+                  fontColor: '#fff',
+                  fontSize: 8,
+                  boxWidth: 8,
+                  boxHeight: 8
+              }
+          }
+      }
+      
+  });
+
+}
+
+
+function amenitiesChart(feature){
+
+    const amenitiesData = [
+        feature.properties.bar,
+        feature.properties.cafe,
+        feature.properties.cinema,
+        feature.properties.clinic,
+        feature.properties.department_store,
+        feature.properties.hospital,
+        feature.properties.major_road,
+        feature.properties.mall,
+        feature.properties.park,
+        feature.properties.restaurant,
+        feature.properties.school,
+        feature.properties.supermarket,
+        feature.properties.worship
+    ]
+
+    const amenitiesLabels = [
+        'bar', 
+        'cafe', 
+        'cinema',
+        'clinic', 
+        'department_store', 
+        'hospital',
+        'major_road',
+        'mall', 
+        'park', 
+        'restaurant',
+        'school', 
+        'supermarket', 
+        'worship'
+    ];
+
+
+    // Get the canvas element
+    const ameChartCanvas = document.getElementById('ameChartCanvas');
+
+    ameChartCanvas.style.display = 'flex';
+    ameChartCanvas.style.height = ameChartCanvas.style.height * 1.5;
+    ameChartCanvas.style.width = ameChartCanvas.style.width *2;
+    // Check if a chart already exists
+    if (window.amenitiesChart) {
+        window.amenitiesChart.destroy();
+    }
+
+    // Create the new chart
+    window.amenitiesChart = new Chart(ameChartCanvas, {
+        type: 'bar',
+        data: {
+        labels: amenitiesLabels,
+        datasets: [{
+            data: amenitiesData,
+            backgroundColor: [
+                '#f7909f', '#e2647f', '#d1698c', '#e8555f', '#da332a', 
+                '#fe672a', '#ff7a34', '#ffb22c', '#83ba69', '#4ca64e',
+                '#2a897e', '#287f91', '#62b0dc']
+        }]
+        },
+        options: {
+            responsive: true,
+            title: {
+                display: true,
+                text: 'Amenities Counts',
+                fontColor: '#fff'
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        fontColor: '#fff'
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        fontColor: '#fff'
+                    }
+                }]
+            },
+            legend: {
+                labels: {
+                    fontColor: '#fff'
+                }
+            }
+        }
+        
+    });
+
+}
+
+
+
 
 export {
     addHexLayer
